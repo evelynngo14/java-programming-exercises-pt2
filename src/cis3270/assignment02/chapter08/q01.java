6540e7eabd93c3b0070f579e39bb1d1c84d99617
evelynngo14 <evelynngo14@gmail.com>
2025-10-28 12:35:08 -0400

    Update q01.java

--- src/cis3270/assignment02/chapter08/q01.java
+++ src/cis3270/assignment02/chapter08/q01.java
@@ -4,21 +4,24 @@ import java.util.Scanner;
 public class q01 {
 
     public static void main(String[] args) {
-
-    }
-    public static double sumColumn(double[][] m, int columnIndex) {
-
-        double[][] m = new int[3][4];
+        double[][] m = new double[3][4];
         System.out.println("Enter a 3-by-4 matrix row by row: ");
         Scanner input = new Scanner(System.in);
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++)
-                m[i][j] = input.nextInt();
-
+                m[i][j] = input.nextDouble();
         }
 
-
-
+        for (int i = 0; i < 4; i++) {
+            System.out.printf("Sum of the elements at column %d: %.1f \n", i, sumColumn(m, i));
+        }
+    }
+    public static double sumColumn(double[][] m, int columnIndex) {
+        double sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += m[i][columnIndex];
+        }
+        return sum;
 
     }
 }
