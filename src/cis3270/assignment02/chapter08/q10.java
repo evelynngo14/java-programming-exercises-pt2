375d0986d496977e3ae1b1da165da353c241aa47
evelynngo14 <evelynngo14@gmail.com>
2025-11-01 00:45:39 -0400

    Complete q10

---
+++ src/cis3270/assignment02/chapter08/q10.java
@@ -0,0 +1,58 @@
+package cis3270.assignment02.chapter08;
+
+public class q10 {
+    public static void main(String[] args) {
+        int[][] matrix = fillMatrix(4,4);
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(matrix[i][j]);
+            }
+            System.out.println();
+        }
+
+        int[] largestIndices = findLargestIndex(matrix);
+        System.out.println("The largest row index: " + largestIndices[0]);
+        System.out.println("The largest column index: " + largestIndices[1]);
+    }
+
+    public static int[][] fillMatrix(int rows, int cols) {
+        int[][] matrix = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = (int) (Math.random() * 2);
+            }
+        }
+        return matrix;
+    }
+
+    public static int[] findLargestIndex(int[][] m) {
+        int largestRowIndex = 0;
+        int largestColIndex = 0;
+        int largestRow = 0;
+        int largestCol = 0;
+
+        for (int i = 0; i < m.length; i++) {
+            int sumRow = 0;
+            for (int j = 0; j < m[i].length; j++) {
+                sumRow += m[i][j];
+            }
+            if (sumRow > largestRow) {
+                largestRow = sumRow;
+                largestRowIndex = i;
+            }
+        }
+        for (int j = 0; j < m[0].length; j++) {
+            int sumCol = 0;
+            for (int i = 0; i < m.length; i ++) {
+                sumCol += m[i][j];
+            }
+            if (sumCol > largestCol) {
+                largestCol = sumCol;
+                largestColIndex = j;
+            }
+        }
+
+        return new int[]{largestRowIndex, largestColIndex};
+    }
+}
