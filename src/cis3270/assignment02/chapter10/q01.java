ffe0c0bb240b25064e137a6178129e92a8885ed7
evelynngo14 <evelynngo14@gmail.com>
2025-11-10 21:04:09 -0500

    Complete chapter09.q01

--- src/cis3270/assignment02/chapter10/q01.java
+++ src/cis3270/assignment02/chapter10/q01.java
@@ -1,4 +1,47 @@
 package cis3270.assignment02.chapter10;
 
 public class q01 {
+    class Time {
+        int hour;
+        int minute;
+        int second;
+
+        Time() {
+            return (System.currentTimeMillis())
+        }
+
+        Time(int hour, int minute, int second) {
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+
+        Time(long elapsedTime) {
+            setTime(elapsedTime);
+        }
+
+        setTime(long elapseTime) {
+            long totalSeconds = elapseTime / 1000;
+            second = (int)(totalSeconds % 60);
+
+            long totalMinutes = totalSeconds / 60;
+            minute = (int)(totalMinutes % 60);
+
+            long totalHours = totalMinutes / 60;
+            hour = (int)(totalHours % 24);
+        }
+
+        public int getHour() {
+            return hour;
+        }
+
+        public int getMinute() {
+            return minute;
+        }
+
+        public int getSecond() {
+            return second;
+        }
+    }
+
 }
---
+++ src/cis3270/assignment02/chapter10/q03.java
@@ -0,0 +1,49 @@
+package cis3270.assignment02.chapter10;
+
+public class q03 {
+    class MyInteger{
+        int value;
+
+        public MyInteger(int value) {
+            this.value = value;
+        }
+
+        public boolean isEven(int value) {
+            return value % 2 == 0;
+
+        }
+
+        public boolean isOdd(int value) {
+            return value % 2 != 0;
+        }
+
+        public boolean isPrime(int value) {
+            if (value <= 1)
+                return false;
+            for (int i = 2; i < value; i++)
+                if (value % i == 0)
+                    return false;
+            return true;
+        }
+
+        public static boolean isEven(MyInteger obj) {
+            return isEven(obj)
+        }
+
+        public static boolean isOdd(MyInteger obj) {
+            return isOdd(obj);
+        }
+
+        public static boolean isPrime(MyInteger obj) {
+            return isEven(obj);
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        MyInteger num = new MyInteger(7);
+        System.out.println(num.isPrime()); // true
+        System.out.println(MyInteger.isEven(num)); // false
+    }
+}
